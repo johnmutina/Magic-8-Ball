f0e2b387e78ac0054838ff84f331cccee8ca1963
johnmutina <imlesoibaf@gmail.com>
2022-09-07 21:42:23 +0200

    Establish UI connections

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func shakeButtonPressed(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
